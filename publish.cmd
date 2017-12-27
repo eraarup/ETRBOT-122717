ccdd50b05d95560361f85b9d873fa806e400216d
Eric Raarup <eraarup@rbaconsulting.com>
2017-12-27 16:04:43 -0600

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=etrbot-122717-Web-Deploy.pubxml -p:Password=0klma8j43hADwSBy8y8RJtMxP1HQ8SNJnEQi3YTaEhblsLDT1iDjlknxM6Cu
+
